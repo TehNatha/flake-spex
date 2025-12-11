c26a4d662b3f1c3c370097394c8fbd22751013ca
Nate S <me@tehnatha.dev>
2025-12-11 05:41:04 -0800

    Tags fix

--- flakeModules/spex/mkSpecificationSubmodule/default.nix
+++ flakeModules/spex/mkSpecificationSubmodule/default.nix
@@ -36,7 +36,7 @@ in submodule ({ config, name, ... }: {
     };
     tags = mkOption {
       type = listOf (uniq str);
-      default = specToTags config.spec ++ config.spec.extraTag;
+      default = specToTags config.spec ++ config.extraTags;
       readOnly = true;
       description = "Computed tags for the specification. This value is automatically computed from `spec` and `extraTags`.";
     };
